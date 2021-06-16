6e0bdda3c84bfb011f309082c0036eae3642e575
Myrmoxenus <scopocira@gmail.com>
2021-06-16 18:21:31 -0500

    Added links to deployed applications

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,32 +3,38 @@ let projectObjects = [
     {
         name: 'Map-Based Recipe Search',
         image: 'portfolioicon1.png',
-        link: 'https://github.com/EgiVilza/NutritiousRecipeGenerator'
+        github: 'https://github.com/EgiVilza/NutritiousRecipeGenerator',
+        link: 'https://egivilza.github.io/NutritiousRecipeGenerator/map.html'
     },
     {
         name: 'Weather Dashboard',
         image: 'portfolioicon2.png',
-        link: 'https://github.com/Myrmoxenus/homework6'
+        github: 'https://github.com/Myrmoxenus/homework6',
+        link: 'https://myrmoxenus.github.io/homework6/'
     }, 
     {
         name: 'Password Generator',
         image: 'portfolioicon3.png',
-        link: 'https://github.com/Myrmoxenus/homework3'
+        github: 'https://github.com/Myrmoxenus/homework3',
+        link: 'https://myrmoxenus.github.io/homework3/'
     }, 
     {
         name: 'Employee Tracker',
         image: 'portfolioicon4.png',
-        link: 'https://github.com/Myrmoxenus/EmployeeTracker'
+        github: 'https://github.com/Myrmoxenus/EmployeeTracker',
+        link: 'https://drive.google.com/drive/folders/1OnADLzX_jZacfchbDS_yCszsTkBkI7e0'
     },
     {
         name: 'Multiplayer Blackjack',
         image: 'portfolioicon5.png',
-        link: 'https://github.com/EgiVilza/Casino_Multiplayer'
+        github: 'https://github.com/EgiVilza/Casino_Multiplayer',
+        link: 'https://casino-multiplayer.herokuapp.com/'
     },
     {
         name: 'Employee Directory',
         image: 'portfolioicon6.png',
-        link: 'https://github.com/Myrmoxenus/EmployeeDirectory'
+        github: 'https://github.com/Myrmoxenus/EmployeeDirectory',
+        link: 'https://employee-directory-homework.herokuapp.com/'
     }
 ]
 
@@ -40,7 +46,8 @@ const Portfolio = () => {
         {projectObjects.map(project => <div className="projectCard">
             <h2>{project.name}</h2>
             <img src={`/images/${project.image}`} className="projectThumbnail" alt={`Screenshot of ${project.name}`}></img>
-            <a href={project.link} className="contactButton" target="_blank" rel="noreferrer"  >GitHub</a>
+            <a href={project.github} className="contactButton" target="_blank" rel="noreferrer"  >GitHub</a>
+            <a href={project.link} className="contactButton" target="_blank" rel="noreferrer"  >Link</a>
         </div>)}
 
         </div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import NavbarAndRouter from './NavbarAndRouter';
-import Footer from './Footer'
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <NavbarAndRouter />
-    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
