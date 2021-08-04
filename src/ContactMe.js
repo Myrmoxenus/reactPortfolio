bce94794f56b8fde90bba78a87007375d7d8a3ec
Myrmoxenus <scopocira@gmail.com>
2021-08-04 18:41:53 -0500

    Made email visible on button.

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -4,7 +4,7 @@ const ContactMe = () => {
     return ( 
         <div className="Container">
             <h1>Contact Me</h1>
-              <a href="mailto:scopocira@gmail.com" className="contactButton" target="_blank" rel="noreferrer"  >E-Mail</a>
+              <a href="mailto:scopocira@gmail.com" className="contactButton" target="_blank" rel="noreferrer"  >`scopocira@gmail.com`</a>
               <a href="https://twitter.com/Myrmoxenus" className="contactButton" target="_blank" rel="noreferrer"  >Twitter</a>
               <a href="https://github.com/Myrmoxenus" className="contactButton" target="_blank" rel="noreferrer"   >GitHub</a>
               <a href="https://www.linkedin.com/in/zack-larsen-622414129/" className="contactButton" target="_blank" rel="noreferrer" >LinkedIn</a>
